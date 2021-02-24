62d678463604a5b978099ceee1c80c9f362c84a5
Gene Callahan <gcallah@mac.com>
2021-02-24 10:17:50 -0500

    Added two functions to sample.py.

--- sample.py
+++ sample.py
@@ -6,10 +6,27 @@ tests to the code.
 """
 
 
+def some_func(x):
+    """
+    This function just exists to show how our tools can work with docstrings.
+    It simply takes its argument `x` and prints it.
+    """
+    print(x)
+
+
+def other_func(y):
+    """
+    **Even** inline documentation will sometimes get out of sync, like this:
+    This function takes its two arguments, `x` and `y`, and multiplies them.
+    """
+    return y + 7
+
+
 def main():
     """
     We'll start with a `main()` function that just returns an error code.
     """
+    some_func(10)
     return 0
 
 
