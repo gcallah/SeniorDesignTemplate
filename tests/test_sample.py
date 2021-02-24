27743a90fb61b65392ea5fd82311d4cb71d28fbd
Gene Callahan <gcallah@mac.com>
2021-02-24 10:51:53 -0500

    Added test for other_func.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -6,7 +6,7 @@ This is the test suite for sample.py.
 from unittest import TestCase
 
 
-from sample import main
+from sample import main, other_func
 
 SUCCESS = 0
 
@@ -14,3 +14,6 @@ SUCCESS = 0
 class SampleTestCase(TestCase):
     def test_main(self):
         self.assertTrue(main() == SUCCESS)
+
+    def test_other_func(self):
+        self.assertEquals(other_func(7), 14)
